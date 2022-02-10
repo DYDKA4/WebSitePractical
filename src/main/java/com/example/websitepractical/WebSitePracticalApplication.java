a0b626d3ac365b8265f7749a2630238f555d0d76
Tulin Dmitry <hijito@yandex.ru>
2022-02-10 11:15:59 +0300

    Initial commit

---
+++ src/main/java/com/example/websitepractical/WebSitePracticalApplication.java
@@ -0,0 +1,13 @@
+package com.example.websitepractical;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSitePracticalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebSitePracticalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/websitepractical/WebSitePracticalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.websitepractical;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSitePracticalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
