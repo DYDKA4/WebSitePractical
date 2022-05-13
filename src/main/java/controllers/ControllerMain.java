b8dd7f9003e128c761ae0142d37d00745103b6c5
Tulin Dmitry <hijito@yandex.ru>
2022-05-13 23:21:13 +0300

    Start

--- src/main/java/controllers/ControllerMain.java
+++ src/main/java/controllers/ControllerMain.java
@@ -2,18 +2,26 @@ package controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.ui.Model;
 
 @Controller
 public class ControllerMain {
 
-    @RequestMapping(value = { "/", "/index"})
-    public ModelAndView index() {
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("index.html");
-        return modelAndView;
+//    @RequestMapping(value = { "/", "/index"})
+//    public ModelAndView index() {
+//        ModelAndView modelAndView = new ModelAndView();
+//        modelAndView.setViewName("main_page.html");
+//        return modelAndView;
+//    }
+
+    @GetMapping("/main_page")
+    public String index(Model model) {
+        return "main_page";
+    }
+
+    @GetMapping("/error")
+    public String error(Model model){
+        return "main_page";
     }
 
 //    @RequestMapping(value = "/allPersons" )
